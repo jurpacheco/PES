6917fcb1a2cf6e3b742ead0aceaa30c3937a9df4
ju.rpacheco <juliana.p2010@aluno.ifsc.edu.br>
2026-08-26 08:51:16 -0300

    List 05: Exercise 06, error corrected.

--- lista05/L05EX06.py
+++ lista05/L05EX06.py
@@ -1,4 +1,7 @@
- https://github.com/jurpacheco/PES.git
+print (f'==================================== lista de exercício 05 ====================================')
+print(f'------------------------------------------- Questão 6 -------------------------------------------\n')
+print ("6 - Crie uma função chamada tempo_total que receba a quantidade de horas e minutos\nque um jovem passou jogando videogame e retorne o total de minutos jogados. Peça ao\nusuário para inserir as horas e minutos, e exiba o tempo total em minutos.\n")
+print(f'\n------------------------------------------------------------------------------------------------\n')
 def total_time(a,b):
     h_to_minute=a*60
     total=h_to_minute+b
