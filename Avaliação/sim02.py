0e6919762e5bd437b1bb386633c5919f6877ebcd
jurpacheco <juliana.p2010@aluno.ifsc.edu.br>
2026-09-08 17:24:16 -0300

    Exercícios par Prova: Exercício do simulado 02, errado.

--- Avaliação/sim02.py
+++ Avaliação/sim02.py
@@ -2,3 +2,27 @@ print('='*25, 'Simulação de Prova', '='*25)
 print('-'*25, 'Exercício 2', '-'*25)
 print("Elabore um algoritmo que leia 15 números de uma cartela de bingo e armazene-os em uma lista.\nAceite apenas números entre 1 e 75 e não permita valores repetidos. Ao final, ordene a lista e\nexiba os números do menor para o maior.\n")
 print('='*50)
+
+'''Elabore um algoritmo que leia 15 números de uma cartela de bingo e armazene-os em uma lista.
+Aceite apenas números entre 1 e 75 e não permita valores repetidos. Ao final, ordene a lista e
+exiba os números do menor para o maior.'''
+i=0
+numbers = []
+print ("Coloque os 15 números da tabela, sem repeti-las: ")
+while i<0:
+    number= int(input(f'→ '))
+    if number >= 1 and number<=75:
+        if number in numbers:
+            print (f'{number} já está na lista.\n')
+        else:
+            numbers.append(number)
+            print(f'cadastrado com sucesso!\n')
+    i=1+i
+else:
+    print ("Número menor que 1 ou maior que 75.")
+
+numbers.sort()
+
+for item in numbers:
+    print (f'- {item}')
+
